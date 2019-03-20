55da2ec853b49163c1ffd27629c16980072970b2
alumno <alumno>
2019-03-20 11:19:30 -0300

    clase 2

---
+++ Rosas.Thomas/Clase_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "Hola";
+            
+            Console.WriteLine(Sello.Imprimir());        
+
+            //Sello.Borrar();
+           
+            //Sello.color = ConsoleColor.Blue;
+
+            //Sello.ImprimirEnColor();            
+
+            //Console.WriteLine(Sello.Imprimir());
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rosas.Thomas/Clase_02/Sello.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_02
+{
+    class  Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+        
+
+        public static string Imprimir()
+        {
+            return ArmarFormatoMensaje();
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+        
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = Sello.color;
+
+            Console.WriteLine(Sello.Imprimir());
+
+            Sello.color = ConsoleColor.White;
+            Console.ForegroundColor = Sello.color;
+        }
+
+        private static string ArmarFormatoMensaje()
+        {
+            int len;           
+           
+            string mensajeSello = "";
+
+            len = Sello.mensaje.Length ;
+            if (len > 0)
+            {
+                string techo = "";
+                len += 2;
+                for (int i = 0; i < len; i++)
+                {
+                    techo += "*";
+                }
+
+                mensajeSello += techo;
+                mensajeSello += "\n*";
+                mensajeSello += Sello.mensaje;
+                mensajeSello += "*\n";
+                mensajeSello += techo;
+
+                
+            }
+
+            return mensajeSello;
+        }
+    }
+}
