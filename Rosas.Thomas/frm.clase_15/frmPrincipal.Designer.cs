e6b26fd5c9985173b4af73eadc46e4f3ebcbeda1
alumno <alumno>
2019-05-15 11:59:10 -0300

    as

---
+++ Rosas.Thomas/frm.clase_15/frmPrincipal.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace frm.clase_15
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lstVisor = new System.Windows.Forms.ListBox();
+            this.btnTraer = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(12, 24);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(251, 20);
+            this.txtValor.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(308, 4);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(164, 26);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "&Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lstVisor
+            // 
+            this.lstVisor.FormattingEnabled = true;
+            this.lstVisor.Location = new System.Drawing.Point(12, 69);
+            this.lstVisor.Name = "lstVisor";
+            this.lstVisor.Size = new System.Drawing.Size(456, 225);
+            this.lstVisor.TabIndex = 2;
+            // 
+            // btnTraer
+            // 
+            this.btnTraer.Location = new System.Drawing.Point(12, 300);
+            this.btnTraer.Name = "btnTraer";
+            this.btnTraer.Size = new System.Drawing.Size(107, 34);
+            this.btnTraer.TabIndex = 3;
+            this.btnTraer.Text = "&Traer";
+            this.btnTraer.UseVisualStyleBackColor = true;
+            this.btnTraer.Click += new System.EventHandler(this.btnTraer_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(308, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(164, 26);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Buscar";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 341);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnTraer);
+            this.Controls.Add(this.lstVisor);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtValor);
+            this.Name = "FrmPrincipal";
+            this.Text = "FrmPrincipal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ListBox lstVisor;
+        private System.Windows.Forms.Button btnTraer;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Rosas.Thomas/frm.clase_15/frmPrincipal.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace frm.clase_15
+{
+    public partial class FrmPrincipal : Form
+    {
+        private string _path;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            this.openFileDialog1.Title = "Seleccione un archivo...";
+            this.openFileDialog1.DefaultExt = ".txt";
+            this.openFileDialog1.AddExtension = true;
+            this.openFileDialog1.Filter = "Archivos *.txt|*.TXT";
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\miArchivo.txt"; //AppDomain.CurrentDomain.BaseDirectory + @"miArchivo.txt";
+                if (_path != null)
+                {
+                    StreamWriter lista = new StreamWriter(_path,true);
+                     lista.Write(txtValor.Text);
+                     lista.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Debes elegir una dirección primero.");
+                }
+                
+                
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("No se pudo escribir el archivo\n" + exc.Message);
+            }
+        }
+
+        private void btnTraer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_path != null)
+                {
+                    lstVisor.Items.Clear();
+                    //string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\miArchivo.txt";
+                    StreamReader leer = new StreamReader(_path);
+
+                    do
+                    {
+                        lstVisor.Items.Add(leer.ReadLine());
+                    } while (!leer.EndOfStream);
+
+                    leer.Close();
+                }
+                else
+                {
+                    if(this.openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        _path = openFileDialog1.FileName;
+                        MessageBox.Show(_path);
+                    }
+                }
+                
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("No se pudo leer el archivo\n" + exc.Message);                
+            }
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+         
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _path = openFileDialog1.FileName;
+                MessageBox.Show(_path);
+            }
+
+
+            
+        }
+    }
+}
